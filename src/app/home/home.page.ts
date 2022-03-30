2890395bdafc259d542fbe5bbd7108efe441ae51
keiryoseli99 <keyorodriguezgo@ittepic.edu.mx>
2022-03-30 00:21:51 -0700

    second commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { IonDatetime } from '@ionic/angular';
-import { format, parseISO } from 'date-fns';
-import { es } from 'date-fns/locale';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,23 +6,7 @@ import { es } from 'date-fns/locale';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  @ViewChild(IonDatetime) datetime: IonDatetime;
-  
-  dateValue = '';
-  dateValue2 = new Date(Date.now()).toISOString();
 
   constructor() {}
 
-  private confirm() {
-    this.datetime.confirm(true);
-  }
-  
-  private reset() {
-    this.datetime.reset(new Date(Date.now()).toISOString());
-    //this.datetime.cancel(true);
-  }
-
-  formatDate(value: string) {
-    return format(parseISO(value), 'PPPP', {locale: es});
-  }
 }
